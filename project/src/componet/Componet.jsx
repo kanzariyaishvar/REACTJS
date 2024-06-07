2404e321682c6398cecc1d3f42e96cea9b54f652
kanzariyaishvar <ishvarkanzariya2@gmail.com>
2024-06-07 19:55:38 +0530

    changes

--- project/src/componet/Componet.jsx
+++ project/src/componet/Componet.jsx
@@ -43,124 +43,111 @@ function AddComponet() {
     });
   }
 
-  function show() {
-
-  }
 
 
-  //   return (
-  //     <div className="App">
-  //       {state=="Addpro" ?
-  //       <div className='parent'>
-  //         <div className='child'>
-
-  //           <h1> Add Product... </h1>
-
-  //           <label htmlFor="">Enter Product Name..</label>
-  //           <input type="text" placeholder='Enter Product Name' onChange={(i) => { setName(i.target.value) }} className='form-control'/>
-
-  //           <label htmlFor="">Enter Product Price..</label>
-  //           <input type="text" placeholder='Enter Product Price' onChange={(i) => { setPrice(i.target.value) }} className='form-control'/>
-
-  //           <label htmlFor="">Enter Product Description..</label>
-  //           <input type="text" placeholder='Enter Product Description' onChange={(i) => { setDescription(i.target.value) }} className='form-control'/>
-
-  //           <label htmlFor="">Enter Product Category..</label>
-  //           <input type="text" placeholder='Enter Product Category' onChange={(i) => { setCategory(i.target.value) }} className='form-control'/>
-
-  //           <label htmlFor="">Enter Product Stock Quntity..</label>
-  //           <input type="text" placeholder='Enter Product Quntity' onChange={(i) => { setStock(i.target.value) }} className='form-control'/>
-
-  //           <label htmlFor="">Select Product image ..</label><br />
-  //           <input type="url" placeholder='Enter image url..' onChange={(i) => { setImage(i.target.value)}}  className='form-control'/>
-
-  //           <button onClick={addpro}>Add Product</button>
-  //           <button onClick={show}>Show Product</button>
-  //         </div>
-  //       </div>
-  //  :
-
-
-  //       {/* <div className="parent1">
-  //           <h1>View Product</h1>
-  //           <div>
-  //             {
-  //               arr.map((val)=>{
-  //                 <h1>{val.name}</h1>
-  //               })
-  //             }
-  //           </div>
-  //       </div> */}
-
-  // <div className="parent1">
-  //         <h1>View Product</h1>
-  //         <div>
-  //           {arr.map((val, index) => (
-  //             <div key={index}>
-  //               <h2>{val.name}</h2>
-  //               <p>{val.price}</p>
-  //               <p>{val.description}</p>
-  //               <p>{val.category}</p>
-  //               <p>{val.stock}</p>
-  //               <img src={val.image} alt={val.name} style={{ width: "100px", height: "100px" }} />
-  //             </div>
-  //           ))}
-  //         </div>
-  //       </div>
-
-  //     </div>
-  //         }
-  //   );
-  // }
-  return (
-    <div className="App">
-      {state === "Addpro" ? (
+    return (
+      <div className="App">
+        
         <div className='parent'>
           <div className='child'>
+
             <h1> Add Product... </h1>
 
-            <label>Enter Product Name..</label>
-            <input type="text" placeholder='Enter Product Name' onChange={(i) => { setName(i.target.value) }} className='form-control' />
+            <label htmlFor="">Enter Product Name..</label>
+            <input type="text" placeholder='Enter Product Name' onChange={(i) => { setName(i.target.value) }} className='form-control'/>
 
-            <label>Enter Product Price..</label>
-            <input type="text" placeholder='Enter Product Price' onChange={(i) => { setPrice(i.target.value) }} className='form-control' />
+            <label htmlFor="">Enter Product Price..</label>
+            <input type="text" placeholder='Enter Product Price' onChange={(i) => { setPrice(i.target.value) }} className='form-control'/>
 
-            <label>Enter Product Description..</label>
-            <input type="text" placeholder='Enter Product Description' onChange={(i) => { setDescription(i.target.value) }} className='form-control' />
+            <label htmlFor="">Enter Product Description..</label>
+            <input type="text" placeholder='Enter Product Description' onChange={(i) => { setDescription(i.target.value) }} className='form-control'/>
 
-            <label>Enter Product Category..</label>
-            <input type="text" placeholder='Enter Product Category' onChange={(i) => { setCategory(i.target.value) }} className='form-control' />
+            <label htmlFor="">Enter Product Category..</label>
+            <input type="text" placeholder='Enter Product Category' onChange={(i) => { setCategory(i.target.value) }} className='form-control'/>
 
-            <label>Enter Product Stock Quantity..</label>
-            <input type="text" placeholder='Enter Product Quantity' onChange={(i) => { setStock(i.target.value) }} className='form-control' />
+            <label htmlFor="">Enter Product Stock Quntity..</label>
+            <input type="text" placeholder='Enter Product Quntity' onChange={(i) => { setStock(i.target.value) }} className='form-control'/>
 
-            <label>Select Product Image..</label><br />
-            <input type="url" placeholder='Enter image url..' onChange={(i) => { setImage(i.target.value) }} className='form-control' />
+            <label htmlFor="">Select Product image ..</label><br />
+            <input type="url" placeholder='Enter image url..' onChange={(i) => { setImage(i.target.value)}}  className='form-control'/>
 
             <button onClick={addpro}>Add Product</button>
-            <button onClick={show}>Show Product</button>
           </div>
         </div>
-      ) : (
-        <div className="parent1">
+
+
+  <div className="parent1">
           <h1>View Product</h1>
           <div>
             {arr.map((val, index) => (
-              <div key={index}>
-                <h2>{val.name}</h2>
-                <p>{val.price}</p>
-                <p>{val.description}</p>
-                <p>{val.category}</p>
-                <p>{val.stock}</p>
-                <img src={val.image} alt={val.name} style={{ width: "100px", height: "100px" }} />
+              <div key={index} className='print'>
+                <div className='printinner'>
+                <div className='product'><img src={val.Image} alt={val.name} style={{ width: "100px", height: "100px" }} /></div>
+                <div className='product'><p><b>Product Name :- </b> <br />{val.name}</p></div>
+                <div className='product'><p><b>Product Price:- </b><br />{val.price}</p></div>
+                <div className='product'><p><b>Product description:- <br /></b> {val.description}</p></div>
+                <div className='product'><p><b>Product Category :- <br /></b> {val.category}</p></div>
+                <div className='product'><p><b>Product Stock :- </b> <br />{val.stock}</p></div>
+                </div>
               </div>
             ))}
           </div>
         </div>
-      )}
-    </div>
-  );
-}
+
+      </div>
+    );
+  }
+
+
+//   return (
+//     <div className="App">
+//       {state === "Addpro" ? (
+//         <div className='parent'>
+//           <div className='child'>
+//             <h1> Add Product... </h1>
+
+//             <label>Enter Product Name..</label>
+//             <input type="text" placeholder='Enter Product Name' onChange={(i) => { setName(i.target.value) }} className='form-control' />
+
+//             <label>Enter Product Price..</label>
+//             <input type="text" placeholder='Enter Product Price' onChange={(i) => { setPrice(i.target.value) }} className='form-control' />
+
+//             <label>Enter Product Description..</label>
+//             <input type="text" placeholder='Enter Product Description' onChange={(i) => { setDescription(i.target.value) }} className='form-control' />
+
+//             <label>Enter Product Category..</label>
+//             <input type="text" placeholder='Enter Product Category' onChange={(i) => { setCategory(i.target.value) }} className='form-control' />
+
+//             <label>Enter Product Stock Quantity..</label>
+//             <input type="text" placeholder='Enter Product Quantity' onChange={(i) => { setStock(i.target.value) }} className='form-control' />
+
+//             <label>Select Product Image..</label><br />
+//             <input type="url" placeholder='Enter image url..' onChange={(i) => { setImage(i.target.value) }} className='form-control' />
+
+//             <button onClick={addpro}>Add Product</button>
+//             <button onClick={show}>Show Product</button>
+//           </div>
+//         </div>
+      
+//         <div className="parent1">
+//           <h1>View Product</h1>
+//           <div>
+//             {arr.map((val, index) => (
+//               <div key={index}>
+//                 <h2>{val.name}</h2>
+//                 <p>{val.price}</p>
+//                 <p>{val.description}</p>
+//                 <p>{val.category}</p>
+//                 <p>{val.stock}</p>
+//                 <img src={val.image} alt={val.name} style={{ width: "100px", height: "100px" }} />
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddComponet;
 
